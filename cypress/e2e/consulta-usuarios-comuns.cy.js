dec5bfeb63225e29490f6c9a6090f4bd5929fada
luanpoppe <luanpoppe@gmail.com>
2024-04-16 15:21:54 -0300

    feat: novas refatorações

--- cypress/e2e/consulta-usuarios-comuns.cy.js
+++ cypress/e2e/consulta-usuarios-comuns.cy.js
@@ -10,38 +10,27 @@ describe('Validação de consultas de usuários comuns', () => {
       email = user.email
       name = user.name
       password = user.password
-    }).then(() => {
-      // Processo para criar e logar com um usuário antes dos testes
-      cy.log("Criar um usuário")
-      cy.request("POST", '/api/users', {
-        name: name,
-        email: email,
-        password: password
-      }).then((resposta) => {
-        id = resposta.body.id
-        email = resposta.body.email
-        name = resposta.body.name
-        cy.log("Logar com usuário criado")
-        cy.request("POST", "/api/auth/login", {
-          email: email,
-          password: "senha123"
-        }).then((resposta) => {
-          token = resposta.body.accessToken
+
+      cy.criarUsuario(name, email, password).then((idRecebido) => {
+        id = idRecebido
+      }).then(function () {
+        cy.logar(email, password).then((tokenRecebido) => {
+          token = tokenRecebido
         })
       })
     })
+
 
   })
 
   after(() => {
     // Processo para apagar o usuário ao fim dos testes
-
     cy.log("Deletar o usuário criado")
     cy.request({
       method: 'DELETE',
-      url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + id,
-      headers: {
-        Authorization: "Bearer " + token
+      url: '/api/users/' + id,
+      auth: {
+        bearer: token
       }
 
     })
@@ -51,19 +40,27 @@ describe('Validação de consultas de usuários comuns', () => {
   it("Consultar informações do usuário criado", () => {
     cy.request({
       method: "GET",
-      url: "https://raromdb-3c39614e42d4.herokuapp.com/api/users/" + id,
-      headers: {
-        Authorization: "Bearer " + token
+      url: "/api/users/" + id,
+      auth: {
+        bearer: token
       }
+    }).then((resposta) => {
+      expect(resposta.status).to.equal(200)
+      expect(resposta.body).to.deep.include({
+        email: email,
+        name: name,
+        active: true,
+        type: 0
+      })
     })
   })
 
   it("Não permitir que usuário do tipo comum tenha permissão de ver informações de todos os usuário", () => {
     cy.request({
       method: "GET",
-      url: "https://raromdb-3c39614e42d4.herokuapp.com/api/users/",
-      headers: {
-        Authorization: "Bearer " + token
+      url: "/api/users/",
+      auth: {
+        bearer: token
       },
       failOnStatusCode: false
     }).then((resposta) => {
@@ -72,12 +69,28 @@ describe('Validação de consultas de usuários comuns', () => {
     })
   })
 
+  it("Não permitir que usuário do tipo comum tenha permissão de ver informações de outro usuário", () => {
+    cy.request({
+      method: "GET",
+      url: "/api/users/" + (id - 1),
+      auth: {
+        bearer: token
+      },
+      failOnStatusCode: false
+    }).then((resposta) => {
+      expect(resposta.status).to.equal(403)
+      expect(resposta.body).to.deep.include({
+        message: "Forbidden"
+      })
+    })
+  })
+
   it("Não permitir que usuário do tipo comum tenha permissão de deletar um usuário", () => {
     cy.request({
       method: "DELETE",
-      url: "https://raromdb-3c39614e42d4.herokuapp.com/api/users/" + id,
-      headers: {
-        Authorization: "Bearer " + token
+      url: "/api/users/" + id,
+      auth: {
+        bearer: token
       },
       failOnStatusCode: false
     }).then((resposta) => {
@@ -89,9 +102,9 @@ describe('Validação de consultas de usuários comuns', () => {
   it("Permitir que usuário do tipo comum atualize suas próprias informações", () => {
     cy.request({
       method: "PUT",
-      url: "https://raromdb-3c39614e42d4.herokuapp.com/api/users/" + (id),
-      headers: {
-        Authorization: "Bearer " + token
+      url: "/api/users/" + (id),
+      auth: {
+        bearer: token
       },
       body: {
         name: "novoNome",
@@ -106,9 +119,9 @@ describe('Validação de consultas de usuários comuns', () => {
   it("Não permitir que usuário do tipo comum atualize informações de outros usuários", () => {
     cy.request({
       method: "PUT",
-      url: "https://raromdb-3c39614e42d4.herokuapp.com/api/users/" + (id - 1),
-      headers: {
-        Authorization: "Bearer " + token
+      url: "/api/users/" + (id - 1),
+      auth: {
+        bearer: token
       },
       body: {
         name: "novoNome",
@@ -130,12 +143,22 @@ describe('Validação de consultas de usuários comuns', () => {
   it("Permitir que usuário comum se torne admin sem passar por critérios especiais", () => {
     cy.request({
       method: 'PATCH',
-      url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/admin/',
-      headers: {
-        Authorization: "Bearer " + token
+      url: '/api/users/admin/',
+      auth: {
+        bearer: token
       }
     }).then((resposta) => {
       expect(resposta.status).to.equal(204)
+
+      cy.request({
+        method: 'GET',
+        url: '/api/users/' + id,
+        auth: {
+          bearer: token
+        },
+      }).then((resposta) => {
+        expect(resposta.body.type).to.equal(1)
+      })
     })
   })
 
