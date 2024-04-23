74dc2da7377df4aa9b0056c39b063f739e490046
luanpoppe <luanpoppe@gmail.com>
2024-04-23 11:19:44 -0300

    feat: melhorias e novos testes em autenticação de usuário

--- cypress/e2e/auth/autenticacao-usuario.cy.js
+++ cypress/e2e/auth/autenticacao-usuario.cy.js
@@ -6,12 +6,11 @@ describe('Validação de cadastro de usuários', () => {
   let password
 
   before(() => {
-    cy.fixture("newUser.json").then((user) => {
+    cy.criarFaker().then((user) => {
       email = user.email
       name = user.name
       password = user.password
     }).then(() => {
-      // Processo para criar um usuário antes dos testes
       cy.log("Criar um usuário")
       cy.criarUsuario(name, email, password).then((resposta) => {
         id = resposta
@@ -20,7 +19,6 @@ describe('Validação de cadastro de usuários', () => {
   })
 
   after(() => {
-    // Processo para apagar o usuário depois de ser cadastrado
     cy.log("Processo de apagar o usuário criado: ")
     cy.logar(email, password).then((resposta) => {
       token = resposta
@@ -28,20 +26,99 @@ describe('Validação de cadastro de usuários', () => {
     })
   })
 
-  it("Não é possível logar com um email não existente", () => {
-    const body = {
-      email: "emailNaoExistente@email.com",
-      password: password
-    }
-    cy.req("POST", "/api/auth/login", body, null, false)
-      .then((resposta) => {
-        expect(resposta.status).to.equal(401)
-        expect(resposta.body.message).to.equal("Invalid username or password.")
-        expect(resposta.body.accessToken).to.not.exist
-      })
+  describe("Falhas ao tentar logar", function () {
+    it("Falha ao não passar o body com atributos esperados pela API", () => {
+      cy.req("POST", "/api/auth/login", null, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.deep.equal([
+            "email should not be empty",
+            "email must be an email",
+            "password must be a string",
+            "password should not be empty"
+          ])
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
+
+    it("Não é possível logar sem passar um email", () => {
+      const body = {
+        password: password
+      }
+      cy.req("POST", "/api/auth/login", body, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.deep.equal(["email should not be empty", "email must be an email"])
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
+
+    it("Não é possível logar com um email não existente", () => {
+      const body = {
+        email: "emailNaoExistente@email.com",
+        password: password
+      }
+      cy.req("POST", "/api/auth/login", body, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(401)
+          expect(resposta.body.message).to.equal("Invalid username or password.")
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
+
+    it("Não é possível logar com um email não sendo uma string", () => {
+      const body = {
+        email: 123456,
+        password: password
+      }
+      cy.req("POST", "/api/auth/login", body, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.deep.equal(["email must be an email"])
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
+
+    it("Não é possível sem passar uma senha", () => {
+      const body = {
+        email: email,
+      }
+      cy.req("POST", "/api/auth/login", body, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.deep.equal(["password must be a string", "password should not be empty"])
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
+
+    it("Não é possível logar com um password sendo string vazia", () => {
+      const body = {
+        email: email,
+        password: ""
+      }
+      cy.req("POST", "/api/auth/login", body, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.deep.equal(["password should not be empty"])
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
+
+    it("Não é possível logar com um password não sendo uma string", () => {
+      const body = {
+        email: email,
+        password: 123456
+      }
+      cy.req("POST", "/api/auth/login", body, null, false)
+        .then((resposta) => {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.deep.equal(["password must be a string"])
+          expect(resposta.body.accessToken).to.not.exist
+        })
+    })
   })
 
-  it('Pessoa não logada não consegue acessar endpoints', () => {
+  it('Pessoa não logada não consegue acessar endpoints que necessitem de autorização', () => {
     cy.req('GET', '/api/users/' + id, null, null, false)
       .then((resposta) => {
         expect(resposta.status).to.equal(401)
@@ -49,7 +126,7 @@ describe('Validação de cadastro de usuários', () => {
       })
   })
 
-  it('Não é possível acessar endpoints com um accessToken errado', () => {
+  it('Não é possível acessar endpoints que necessitem de autorização com um accessToken errado', () => {
     cy.req('GET', '/api/users/' + id, null, "token falso", false)
       .then((resposta) => {
         expect(resposta.status).to.equal(401)
